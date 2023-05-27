5fda725688b8026498d71dfd397fed0a0df76cb9
AVINASH KUMAR <65335071+avipandit@users.noreply.github.com>
2023-05-27 13:57:17 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,21 @@
 
+import { useState } from "react";
 import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+  let [text, setText] = useState('');
+  let textTemp = "";
+  function textAdd(){
+    textTemp = document.getElementById('a').value;
+    setText(textTemp);
+  }
   return (
     <div>
-        {/* Do not remove the main div */}
+        <p>Enter your name:</p>
+        <input onChange={textAdd} id="a"></input>
+        <p id="p">Hello {text}!</p>
     </div>
   )
 }
-
 export default App
